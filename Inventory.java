c1d8dfa459436cac8a4bc2dbf304144d009354dc
Virginia Estes <40926700+vestes22@users.noreply.github.com>
2019-06-05 19:21:37 -0600

    Create Inventory.java

---
+++ Inventory.java
@@ -0,0 +1,83 @@
+package c482project0;
+
+import javafx.collections.ObservableList;
+
+public class Inventory 
+{
+    private ObservableList<Parts> allParts;
+    private ObservableList<Products> allProducts;
+    
+    
+    public void addPart(Parts part)
+    {
+        
+       
+    }
+    
+    /*
+    public void addProduct(Products product)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public Parts searchParts(int partId)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public Products searchProducts(int productId)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public ObservableList<Parts> searchPart(String partName)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public ObservableList<Products> searchProduct(String productName)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public void updatePart(int index, Parts part)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public void updateProduct(int index, Products product)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public void deletePart(Parts part)
+    {
+        //FIXME
+    }
+    */
+    /*
+    public void deleteProduct(Products product)
+    {
+        //FIXME
+    }
+    */
+    
+    public ObservableList<Parts> getAllParts()
+    {
+        return allParts;
+    }
+    
+    
+    public ObservableList<Products> getAllProducts()
+    {
+        return allProducts;
+    }
+    
+}
