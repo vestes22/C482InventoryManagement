b203ab27c2f064aef053858e81a550e9e77c6128
Virginia Estes <40926700+vestes22@users.noreply.github.com>
2019-12-11 15:27:45 -0700

    Logic Bug Fixes
    Fixed bug allowing inventory levels to be set between Max and Min values.
    Fixed bug allowing Min values to be set above Max values.
    Added individualized error messages when constraints are violated.

--- Controller/AddProductController.java
+++ Controller/AddProductController.java
@@ -1,11 +1,12 @@
 package Controller;
 
-import Model.InHouse;
 import Model.Inventory;
-import Model.Outsourced;
 import Model.Part;
+import Model.Product;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,101 +14,131 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-
-public class ModifyPartController implements Initializable
+public class AddProductController implements Initializable
 {
     Stage stage;
     Parent scene;
     
-    @FXML
-    private TextField modifyPartIdText;
-
-    @FXML
-    private TextField modifyPartNameText;
-
-    @FXML
-    private TextField modifyPartInvText;
-
-    @FXML
-    private TextField modifyPartPriceText;
-
-    @FXML
-    private TextField modifyPartMaxText;
-
-    @FXML
-    private TextField modifyPartMinText;
-
-    @FXML
-    private Button modifyPartCancelButton;
-
-    @FXML
-    private Button modifyPartSaveButton;
-
-    @FXML
-    private RadioButton modifyPartInHouseRb;
-
-    @FXML
-    private ToggleGroup modifyPartToggleGroup;
-
-    @FXML
-    private RadioButton modifyPartOutsourcedRb;
+    private ObservableList<Part> searchedParts = FXCollections.observableArrayList();
+    private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
     
     @FXML
-    private Label outsourcedOrInHouseLabel;
+    private TextField addProdIdText;
+
+    @FXML
+    private TextField addProdNameText;
+
+    @FXML
+    private TextField addProdInvText;
+
+    @FXML
+    private TextField addProdPriceText;
+
+    @FXML
+    private TextField addProdMinText;
+
+    @FXML
+    private TextField addProdMaxText;
+
+    @FXML
+    private Button addProdSearchButton;
+
+    @FXML
+    private Button addProdAddButton;
+
+    @FXML
+    private Button addProdDeleteButton;
+
+    @FXML
+    private Button addProdCancelButton;
+
+    @FXML
+    private Button addProdSaveButton;
+
+    @FXML
+    private TextField addProdSearchText;
+
+    @FXML
+    private TableView<Part> addProdTopTable;
+
+    @FXML
+    private TableColumn<Part, Integer> topAddProdIdCol;
+
+    @FXML
+    private TableColumn<Part, String> topAddProdNameCol;
+
+    @FXML
+    private TableColumn<Part, Integer> topAddProdInvCol;
+
+    @FXML
+    private TableColumn<Part, Double> topAddProdPriceCol;
+
+    @FXML
+    private TableView<Part> addProdBottomTable;
+
+    @FXML
+    private TableColumn<Part, Integer> bottomAddProdIdCol;
+
+    @FXML
+    private TableColumn<Part, String> bottomAddProdNameCol;
+
+    @FXML
+    private TableColumn<Part, Integer> bottomAddProdInvCol;
+
+    @FXML
+    private TableColumn<Part, Double> bottomAddProdPriceCol;
     
     @FXML
-    private TextField outsourcedOrInHouseText; 
-    
-    @FXML 
     private Label warningLabel;
     
-    @FXML
-    void inHouseSelected(MouseEvent event) 
-    {
-        outsourcedOrInHouseLabel.setText("Machine ID");
-    }
+    @FXML 
+    private Label productWarningLabel;
     
-    @FXML
-    void outsourcedSelected(MouseEvent event) 
-    {
-        outsourcedOrInHouseLabel.setText("Company Name");
-    }
     
-    public void selectModifiedPart(Part part)
-    {
-        modifyPartIdText.setText(String.valueOf(part.getId()));
-        modifyPartNameText.setText(part.getName());
-        modifyPartInvText.setText(String.valueOf(part.getStock()));
-        modifyPartPriceText.setText(String.valueOf(part.getPrice()));
-        modifyPartMaxText.setText(String.valueOf(part.getMax()));
-        modifyPartMinText.setText(String.valueOf(part.getMin()));
-        
-        
-        if (part instanceof InHouse)
-        {
-            InHouse inHousePart = (InHouse) part;
-            modifyPartInHouseRb.setSelected(true);
-            outsourcedOrInHouseLabel.setText("Machine ID");
-            outsourcedOrInHouseText.setText(String.valueOf(inHousePart.getMachineId()));
-        }
-        
-        if (part instanceof Outsourced)
-        {
-            Outsourced outsourcedPart = (Outsourced) part;
-            modifyPartOutsourcedRb.setSelected(true);
-            outsourcedOrInHouseLabel.setText("Company Name");
-            outsourcedOrInHouseText.setText(String.valueOf(outsourcedPart.getCompanyName()));
-        }
-    }
 
     @FXML
-    void modifyPartCancelButtonClicked(MouseEvent event) throws Exception
+    void addProdAddButtonClicked(MouseEvent event) 
+    {
+        productWarningLabel.setText("");
+        Part addedPart = addProdTopTable.getSelectionModel().getSelectedItem();
+        if (isPresent(addedPart.getId(), associatedParts))
+        {
+            productWarningLabel.setText("That part is already associated with this product.");
+        }
+        else 
+        {
+            associatedParts.add(addedPart);
+        }
+        addProdBottomTable.setItems(associatedParts);
+        bottomAddProdIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        bottomAddProdNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        bottomAddProdInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        bottomAddProdPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+     
+    }
+
+    //Searches whether a Part exists based on ID.
+    public boolean isPresent(int id, ObservableList<Part> parts)
+    {
+       for (Part part : parts)
+       {
+           if (part.getId() == id)
+           {
+               return true;
+           }
+       }
+       return false;
+    }
+    
+    @FXML
+    void addProdCancelButtonClicked(MouseEvent event) throws Exception
     {
         stage = (Stage)((Button)event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
@@ -116,75 +147,95 @@ public class ModifyPartController implements Initializable
     }
 
     @FXML
-    void modifyPartSaveButtonClicked(MouseEvent event) throws Exception
+    void addProdDeleteButtonClicked(MouseEvent event) 
     {
-        try 
+        int index = addProdBottomTable.getSelectionModel().getSelectedIndex();
+        associatedParts.remove(index);
+        addProdBottomTable.setItems(associatedParts);
+        bottomAddProdIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        bottomAddProdNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        bottomAddProdInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        bottomAddProdPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
+
+    @FXML
+    void addProdSaveButtonClicked(MouseEvent event) 
+    {
+        try
         {
-            int index = -1;
-            Part newPart;
-            int id = Integer.parseInt(modifyPartIdText.getText());
-            String name = modifyPartNameText.getText();
-            int inv = Integer.parseInt(modifyPartInvText.getText());
-            double price = Double.parseDouble(modifyPartPriceText.getText());
-            int max = Integer.parseInt(modifyPartMaxText.getText());
-            int min = Integer.parseInt(modifyPartMinText.getText());
-        
-            //Finds the ObservableArray index of the part being modified by matching the Part ID, so we can replace it.
-            for (int i = 0; i < Inventory.getAllParts().size(); i++)
+            String name = addProdNameText.getText();
+            int inv = Integer.parseInt(addProdInvText.getText());
+            double price = Double.parseDouble(addProdPriceText.getText());
+            int max = Integer.parseInt(addProdMaxText.getText());
+            int min = Integer.parseInt(addProdMinText.getText());
+            Product product = new Product(Product.productIdGenerator(), name, price, inv, max, min);
+            for (Part part : associatedParts)
             {
-                if (Inventory.getAllParts().get(i).getId() == id)
-                {
-                    index = i;
-                }
+                product.addAssociatedPart(part);
             }
-        
-            if (name.equals(""))
+            
+            if(min > max)
             {
-                warningLabel.setText("*All fields must be completed before saving.");
+                productWarningLabel.setText("*Min values cannot be greater than Max values.");
             }
+            
+            else if (name.equals(""))
+            {
+                productWarningLabel.setText("*All fields must be completed before saving.");
+            }
+            
+            else if (inv > max || inv < min)
+            {
+                productWarningLabel.setText("*Inventory must be between Max and Min values.");
+            }
+            
             else
-            { 
-                if (modifyPartInHouseRb.isSelected())
-                {
-                    int machId = Integer.parseInt(outsourcedOrInHouseText.getText());
-                    newPart = new InHouse(id, name, price, inv, max, min, machId);
-                    Inventory.updatePart(index, newPart);
-                    
-                    stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-                    scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
-                    stage.setScene(new Scene(scene));
-                    stage.show();
-                }
-                if (modifyPartOutsourcedRb.isSelected())
-                {
-                    String companyName = outsourcedOrInHouseText.getText();
-                    if (companyName.equals(""))
-                    {
-                        warningLabel.setText("*All fields must be completed before saving");
-                    }
-                    else
-                    {
-                        newPart = new Outsourced(Part.partIdGenerator(), name, price, inv, max, min, companyName);
-                        Inventory.updatePart(index, newPart);
-                        
-                        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-                        scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
-                        stage.setScene(new Scene(scene));
-                        stage.show();
-                    }
-                }
+            {
+                Inventory.addProduct(product);
+                associatedParts.clear();
+        
+                stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+                scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
+                stage.setScene(new Scene(scene));
+                stage.show();
             }
+        }
+        catch (Exception e)
+        {
+            productWarningLabel.setText("*All fields must be completed before saving.");
+        }    
+    }
+
+    @FXML
+    void addProdSearchButtonClicked(MouseEvent event)
+    {
+        searchedParts.clear();
+        warningLabel.setText("");
+        try
+        {
+            int id = Integer.parseInt(addProdSearchText.getText());
+            searchedParts.add(Inventory.lookupPart(id));
+            addProdTopTable.setItems(searchedParts);
+            topAddProdIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+            topAddProdNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+            topAddProdInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+            topAddProdPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
         }
         catch(Exception e)
         {
-            warningLabel.setText("*All fields must be completed before saving.");
+            warningLabel.setText("Sorry, we couldn't find that part.");
         }
     }
-    
-    @Override
+
+    @Override 
     public void initialize(URL url, ResourceBundle rb)
     {
-        modifyPartIdText.setDisable(true);
+        addProdIdText.setDisable(true);
+        addProdTopTable.setItems(Inventory.getAllParts());
+        topAddProdIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        topAddProdNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        topAddProdInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        topAddProdPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
     }
-
+    
 }
