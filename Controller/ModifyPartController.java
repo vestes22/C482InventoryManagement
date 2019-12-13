b23fff9176c5668d397b7782e4c1e73807df56ca
Virginia Estes <40926700+vestes22@users.noreply.github.com>
2019-12-12 17:40:42 -0700

    Update ModifyPartController.java

--- Controller/ModifyPartController.java
+++ Controller/ModifyPartController.java
@@ -5,17 +5,19 @@ import Model.Inventory;
 import Model.Outsourced;
 import Model.Part;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -44,16 +46,7 @@ public class ModifyPartController implements Initializable
     private TextField modifyPartMinText;
 
     @FXML
-    private Button modifyPartCancelButton;
-
-    @FXML
-    private Button modifyPartSaveButton;
-
-    @FXML
     private RadioButton modifyPartInHouseRb;
-
-    @FXML
-    private ToggleGroup modifyPartToggleGroup;
 
     @FXML
     private RadioButton modifyPartOutsourcedRb;
@@ -64,9 +57,9 @@ public class ModifyPartController implements Initializable
     @FXML
     private TextField outsourcedOrInHouseText; 
     
-    @FXML 
-    private Label warningLabel;
-    
+    /*
+    * Displays correct fields depending on which RadioButton is selected (InHouse or Outsourced).
+    */
     @FXML
     void inHouseSelected(MouseEvent event) 
     {
@@ -78,7 +71,127 @@ public class ModifyPartController implements Initializable
     {
         outsourcedOrInHouseLabel.setText("Company Name");
     }
+
+    /*
+    * Asks user if they want to exit without saving.
+    * Redirects to the Main Screen.
+    */
+    @FXML
+    void modifyPartCancelButtonClicked(MouseEvent event) throws Exception
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText("Are you sure you want to exit without saving?");
+        Optional<ButtonType> answer = alert.showAndWait();
+        
+        if(answer.isPresent() && answer.get() == ButtonType.OK)
+        {
+            stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/View/MainScreenFXML.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+        } 
+    }
+
+    /*
+    * Updates and saves changes to the Part in Inventory.
+    * Generates an alert box if changes do not conform to constraints.
+    */
+    @FXML
+    void modifyPartSaveButtonClicked(MouseEvent event) throws Exception
+    {
+        try 
+        {
+            int index = -1;
+            Part newPart;
+            int id = Integer.parseInt(modifyPartIdText.getText());
+            String name = modifyPartNameText.getText();
+            int inv = Integer.parseInt(modifyPartInvText.getText());
+            double price = Double.parseDouble(modifyPartPriceText.getText());
+            int max = Integer.parseInt(modifyPartMaxText.getText());
+            int min = Integer.parseInt(modifyPartMinText.getText());
+        
+            //Finds the ObservableArray index of the part being modified by matching the Part ID, so we can replace it.
+            for (int i = 0; i < Inventory.getAllParts().size(); i++)
+            {
+                if (Inventory.getAllParts().get(i).getId() == id)
+                {
+                    index = i;
+                }
+            }
+        
+            if(min > max)
+            {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("WARNING: Invalid Values");
+                alert.setContentText("Min values cannot be greater than Max values.");
+                alert.showAndWait();
+            }
+            
+            else if (name.equals(""))
+            {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("WARNING: Empty Fields");
+                alert.setContentText("All fields must be completed before saving.");
+                alert.showAndWait();
+            }
+            
+            else if (inv > max || inv < min)
+            {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("WARNING: Invalid Values");
+                alert.setContentText("Inventory must be between Max and Min values.");
+                alert.showAndWait();
+            }
+            
+            else
+            { 
+                if (modifyPartInHouseRb.isSelected())
+                {
+                    int machId = Integer.parseInt(outsourcedOrInHouseText.getText());
+                    newPart = new InHouse(id, name, price, inv, min, max, machId);
+                    Inventory.updatePart(index, newPart);
+                    
+                    stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+                    scene = FXMLLoader.load(getClass().getResource("/View/MainScreenFXML.fxml"));
+                    stage.setScene(new Scene(scene));
+                    stage.show();
+                }
+                if (modifyPartOutsourcedRb.isSelected())
+                {
+                    String companyName = outsourcedOrInHouseText.getText();
+                    if (companyName.equals(""))
+                    {
+                        Alert alert = new Alert(Alert.AlertType.WARNING);
+                        alert.setTitle("WARNING: Empty Fields");
+                        alert.setContentText("All fields must be completed before saving.");
+                        alert.showAndWait();
+                    }
+                    else
+                    {
+                        newPart = new Outsourced(Part.partIdGenerator(), name, price, inv, min, max, companyName);
+                        Inventory.updatePart(index, newPart);
+                        
+                        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+                        scene = FXMLLoader.load(getClass().getResource("/View/MainScreenFXML.fxml"));
+                        stage.setScene(new Scene(scene));
+                        stage.show();
+                    }
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING: Empty Fields");
+            alert.setContentText("All fields must be completed before saving.");
+            alert.showAndWait();
+        }
+    }
     
+    /*
+    * Allows us to use an instance of ModifyPartController to pass information from the Main Screen to the Modify Part screen.
+    * Allows us to parse and pre-fill data for the Part to be modified.
+    */
     public void selectModifiedPart(Part part)
     {
         modifyPartIdText.setText(String.valueOf(part.getId()));
@@ -106,92 +219,6 @@ public class ModifyPartController implements Initializable
         }
     }
 
-    @FXML
-    void modifyPartCancelButtonClicked(MouseEvent event) throws Exception
-    {
-        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
-        stage.setScene(new Scene(scene));
-        stage.show();
-    }
-
-    @FXML
-    void modifyPartSaveButtonClicked(MouseEvent event) throws Exception
-    {
-        try 
-        {
-            int index = -1;
-            Part newPart;
-            int id = Integer.parseInt(modifyPartIdText.getText());
-            String name = modifyPartNameText.getText();
-            int inv = Integer.parseInt(modifyPartInvText.getText());
-            double price = Double.parseDouble(modifyPartPriceText.getText());
-            int max = Integer.parseInt(modifyPartMaxText.getText());
-            int min = Integer.parseInt(modifyPartMinText.getText());
-        
-            //Finds the ObservableArray index of the part being modified by matching the Part ID, so we can replace it.
-            for (int i = 0; i < Inventory.getAllParts().size(); i++)
-            {
-                if (Inventory.getAllParts().get(i).getId() == id)
-                {
-                    index = i;
-                }
-            }
-        
-            if(min > max)
-            {
-                warningLabel.setText("*Min values cannot be greater than Max values.");
-            }
-            
-            else if (name.equals(""))
-            {
-                warningLabel.setText("*All fields must be completed before saving.");
-            }
-            
-            else if (inv > max || inv < min)
-            {
-                warningLabel.setText("*Inventory must be between Max and Min values.");
-            }
-            
-            else
-            { 
-                if (modifyPartInHouseRb.isSelected())
-                {
-                    int machId = Integer.parseInt(outsourcedOrInHouseText.getText());
-                    newPart = new InHouse(id, name, price, inv, max, min, machId);
-                    Inventory.updatePart(index, newPart);
-                    
-                    stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-                    scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
-                    stage.setScene(new Scene(scene));
-                    stage.show();
-                }
-                if (modifyPartOutsourcedRb.isSelected())
-                {
-                    String companyName = outsourcedOrInHouseText.getText();
-                    if (companyName.equals(""))
-                    {
-                        warningLabel.setText("*All fields must be completed before saving");
-                    }
-                    else
-                    {
-                        newPart = new Outsourced(Part.partIdGenerator(), name, price, inv, max, min, companyName);
-                        Inventory.updatePart(index, newPart);
-                        
-                        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-                        scene = FXMLLoader.load(getClass().getResource("/view/MainScreenFXML.fxml"));
-                        stage.setScene(new Scene(scene));
-                        stage.show();
-                    }
-                }
-            }
-        }
-        catch(Exception e)
-        {
-            warningLabel.setText("*All fields must be completed before saving.");
-        }
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
